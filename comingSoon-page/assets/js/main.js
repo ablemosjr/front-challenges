31ba93a54291d1b60d2e989d49dd7ac3b587b06f
Alessandro B Lemos Jr <ablemosjunior@gmail.com>
2023-01-11 15:17:07 -0300

    estilo desktop 768px mais, validação de email

--- comingSoon-page/assets/js/main.js
+++ comingSoon-page/assets/js/main.js
@@ -1,2 +1,23 @@
 
-console.log('Ok');
+const inputEmail = document.getElementById('inputEmail');
+const btnSendEmail = document.getElementById('sendEmail');
+const email_error = document.getElementById('email_error');
+
+btnSendEmail.addEventListener('click', () => {
+  const email = inputEmail.value;
+
+  if (!validateEmail(email)) {
+    email_error.innerHTML = 'Please provide a valid email';
+    email_error.classList.add('--has-error');
+    btnSendEmail.classList.add('--has-error');
+    
+  } else {
+    email_error.classList.remove('--has-error');
+    btnSendEmail.classList.remove('--has-error');
+  }
+});
+
+function validateEmail(email) {
+  const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  return re.test(email);
+}
